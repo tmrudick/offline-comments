938eef52f708bd4ffa96c66038e962a6da9b5370
Tom Rudick <tmrudick@gmail.com>
2015-10-10 21:17:11 -0400

    Able to save comments

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var fs = require('fs'),
   http = require('http'),
   path = require('path'),
-  handlebars = require('handlebars');
+  handlebars = require('handlebars'),
+  caser = require('change-case');
 
 // Load templates
-var index = handlebars.compile(fs.readFileSync('index.html.hbs', 'utf-8'));
+var index = handlebars.compile(fs.readFileSync('index.html.hbs', 'utf-8')),
+  review = handlebars.compile(fs.readFileSync('review.html.hbs', 'utf-8')),
+  feedback = handlebars.compile(fs.readFileSync('review.html.hbs', 'utf-8'));
 
 var dir = process.argv[2],
   project = process.argv[3],
@@ -18,15 +21,58 @@ fs.readdirSync(dir).forEach(function(filename, idx) {
 
 var server = http.createServer(function(req, res) {
   console.log(req.method + " " + req.url);
+  switch (req.method) {
+    case 'GET':  
+      handleGet(req, res);
+      break;
+    case 'POST':
+      handlePost(req, res);
+  }
+});
+
+function handleGet(req, res) {
   if (req.url.indexOf('/review/') == 0) {
     var id = req.url.substring('/review/'.length);
-    var contents = fs.readFileSync(path.join(dir, files[id]));
-    res.end(contents);
+    var filename = files[id];
+    res.end(review({
+      code: fs.readFileSync(path.join(dir, filename)),
+      project: project,
+      student: getStudentName(filename),
+      editing: true,
+      fileId: id,
+      comments: "\"{}\""
+    }));
   } else {
     res.end(index({files:files}));    
   }
-})
+}
 
 server.listen(8080, function(err) {
   console.log("Open your browser to http://localhost:8080");
 });
+
+function handlePost(req, res) {
+  var body = "";
+  req.on('data', function(chunk) {
+    body += chunk;
+  });
+
+  req.on('end', function() {
+    var id = req.url.substring('/review/'.length);
+    var filename = files[id];
+    console.log(dir, filename, id);
+    fs.writeFileSync(path.join(dir, filename + '.html'), review({
+      code: fs.readFileSync(path.join(dir, filename)),
+      project: project,
+      student: getStudentName(filename),
+      editing: false,
+      comments: JSON.stringify(body),
+      fileId: id
+    }));
+    res.end();
+  });
+}
+
+function getStudentName(filename) {
+  return caser.titleCase(filename.split('_')[0]);
+}
